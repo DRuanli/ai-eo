cf612d89239b797684c3548bdc565ce095a8c702
Elio <nguyen2332004@gmail.com>
2025-05-06 07:31:09 +0700

    fix

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,5 @@
 <?php
 // Application configuration
 define('APPROOT', dirname(dirname(__FILE__)));
-define('URLROOT', 'http://localhost/ielts-tracker');
+define('URLROOT', 'http://localhost:81/ielts-tracker');
 define('SITENAME', 'IELTS Study Tracker');
